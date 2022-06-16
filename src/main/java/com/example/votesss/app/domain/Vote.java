93b48a9063a64f2451e7316ac56359ae02c26ef0
sidoryak <sidoryak@gmail.com>
2022-06-16 23:09:15 +0700

    SQL optimization, Lombok

--- src/main/java/com/example/votesss/app/domain/Vote.java
+++ src/main/java/com/example/votesss/app/domain/Vote.java
@@ -2,24 +2,30 @@ package com.example.votesss.app.domain;
 
 
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.Type;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
 @Table (name = "votes")
-
+@Check(constraints = "vote_value in ('Y', 'N')")
+@EqualsAndHashCode (onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class Vote {
 
+    @EqualsAndHashCode.Include
     @Id
+    @Type(type = "uuid-char")
+    @Column (length = 36)
     private UUID userId;
-    @Column (updatable = false)
+
+    @Column (length = 1, updatable = false, nullable = false)
+    @Enumerated(EnumType.STRING)
     private VoteValue voteValue;
 }
--- src/main/java/com/example/votesss/app/service/VoteService.java
+++ src/main/java/com/example/votesss/app/service/VoteService.java
@@ -3,7 +3,6 @@ package com.example.votesss.app.service;
 import com.example.votesss.app.domain.Vote;
 import com.example.votesss.app.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
